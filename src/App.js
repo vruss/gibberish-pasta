b60428d98c653918a71e21873fb2006ed3120443
Viktor Rosvall <virosen@yahoo.se>
2019-12-26 12:07:56 +0100

    crude first working gibberish translation

--- src/App.js
+++ src/App.js
@@ -1,43 +1,53 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
-import { googleTranslate } from "./utils/googleTranslate";
-import { Form } from "./form/Form";
+import {googleTranslate} from "./utils/googleTranslate";
+import {Form} from "./form/Form";
 
 import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { translatedString: "" };
+    constructor(props) {
+        super(props);
+        this.state = {translatedString: "", codeCounter: 0};
+        this.codes = ["af", "am", "ar", "az", "be", "bg", "bn", "bs", "ca", "en"];
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.getTranslation = this.getTranslation.bind(this);
+    }
+    
+    handleSubmit(text) {
+        this.getTranslation(text);
+    }
 
-  handleSubmit(text) {
-    this.getTranslation(text);
-  }
+    getTranslation(text) {
+        console.log(this.state.codeCounter);
+        console.log(text);
+        googleTranslate.translate(
+            text,
+            this.codes[this.state.codeCounter],
+            function (err, translation) {
+                this.setState((prevState, props) => ({
+                    codeCounter: prevState.codeCounter + 1
+                }));
+                console.log(translation.translatedText);
+                if (this.state.codeCounter < this.codes.length) {
+                    this.getTranslation(translation.translatedText);
+                }
+                this.setState({translatedString: translation.translatedText});
+            }.bind(this)
+        );
+    }
 
-  getTranslation(text) {
-    googleTranslate.translate(
-      text,
-      "sv",
-      function(err, translation) {
-        console.log(translation.translatedText);
-        this.setState({ translatedString: translation.translatedText });
-      }.bind(this)
-    );
-  }
-
-  render() {
-    return (
-      <div className="Aligner">
-        <Form
-          translatedText={this.state.translatedString}
-          onSubmit={this.handleSubmit}
-        ></Form>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Aligner">
+                <Form
+                    translatedText={this.state.translatedString}
+                    onSubmit={this.handleSubmit}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
