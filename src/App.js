207a08a10e8538ffa28676bfce2cb918e9a498b2
Viktor Rosvall <virosen@yahoo.se>
2019-12-27 16:55:22 +0100

    Working recaptcha, and added env example file

--- src/App.js
+++ src/App.js
@@ -7,22 +7,24 @@ import ReCAPTCHA from "react-google-recaptcha";
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {translatedString: ""};
+        this.state = {translatedString: "", captchaToken: ""};
 
         this.codeCounter = 0;
         this.codes = ["en", "af", "am", "ar", "az", "be", "bg", "bn", "bs", "ca", "en"];
 
         this.handleSubmit = this.handleSubmit.bind(this);
         this.getTranslation = this.getTranslation.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.passedRecaptcha = this.passedRecaptcha.bind(this);
     }
 
     handleSubmit(text) {
         this.getTranslation(text);
     }
 
-
     onChange(value) {
-        console.log("Captcha value:", value);
+        // console.log("Captcha value:", value);
+        this.setState({captchaToken: value});
     }
 
     getTranslation(text) {
@@ -43,16 +45,19 @@ class App extends Component {
         );
     }
 
+    passedRecaptcha() {
+        return this.state.captchaToken !== "";
+    }
+
     render() {
         return (
             <div className="Aligner">
-                <ReCAPTCHA
-                    sitekey={process.env.REACT_APP_GOOGLE_RECAPTCHA_SITE_KEY}
-                    onChange={this.onChange}/>
-                <Form
+                {this.passedRecaptcha() ? <Form
                     translatedText={this.state.translatedString}
                     onSubmit={this.handleSubmit}
-                />
+                /> : <ReCAPTCHA
+                    sitekey={process.env.REACT_APP_GOOGLE_RECAPTCHA_SITE_KEY}
+                    onChange={this.onChange}/>}
             </div>
         );
     }
